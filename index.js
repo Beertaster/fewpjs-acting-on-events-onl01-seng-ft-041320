4a7d45274704460e6209ec725ee657b4ddd01cf0
William Stevens <wstevens.hornet@gmail.com>
2020-07-20 20:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function moveDodgerRight() {
   }
 }
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
