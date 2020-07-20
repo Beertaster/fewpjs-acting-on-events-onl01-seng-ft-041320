4c7be7eeefaffc1c625e0e1c53602340c1e41344
William Stevens <wstevens.hornet@gmail.com>
2020-07-20 20:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`;
+  }
 }
